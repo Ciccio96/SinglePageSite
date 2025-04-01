8edfadd32095ceb74cf016ea4af8a88c4b3adf0a
Francesco Maurici <francescomaurici@iMac-di-Francesco.local>
2025-04-01 16:46:11 +0200

    PR 4

--- singlePage2.js
+++ singlePage2.js
@@ -1,5 +1,17 @@
 // Funzione per aprire/chiudere il menu mobile
-    function toggleMenu() {
-        const menu = document.querySelector('.mobile-menu');
-        menu.classList.toggle('active');
-    }
+function toggleMenu() {
+    const menu = document.querySelector('.mobile-menu');
+    const hamburger = document.querySelector('.hamburger');
+    const close = document.querySelector('.close');
+    const body = document.body;
+
+    // Aggiungi o rimuovi la classe active al menu
+    menu.classList.toggle('active');
+    
+    // Mostra o nascondi l'icona hamburger e la X
+    hamburger.classList.toggle('active');
+    close.classList.toggle('active');
+
+    // Aggiungi o rimuovi la classe per disabilitare lo scroll
+    body.classList.toggle('active-menu');
+}
